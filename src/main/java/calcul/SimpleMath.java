5a2e3e8263f29c37d3e47d3ce6ad096b2bd90c19
lena <flenartowski@proximus.be>
2018-01-08 10:08:10 +0100

    1er commit(début projet)

---
+++ src/main/java/calcul/SimpleMath.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcul;
+
+/**
+ *
+ * @author francoise
+ */
+public class SimpleMath {
+    static public int add(int a, int b)
+    {
+        
+        return a+b;
+    }
+    static public int multiply(int a, int b)
+    {
+        return a*b;
+    }
+     static public int divide(int a, int b)
+    {
+       
+            return a/b;
+       
+    }
+     
+}
---
+++ src/test/java/calcul/SimpleMathTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcul;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author francoise
+ */
+public class SimpleMathTest {
+    
+    public SimpleMathTest() {
+    }
+
+    /**
+     * Test of add method, of class SimpleMath.
+     */
+    @org.junit.Test
+    public void testAdd() {
+        System.out.println("add");
+        int a = 2;
+        int b = 3;
+        int expResult = 5;
+        int result = SimpleMath.add(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of multiply method, of class SimpleMath.
+     */
+    @org.junit.Test
+    public void testMultiply() {
+        System.out.println("multiply");
+        int a = 3;
+        int b = 2;
+        int expResult = 6;
+        int result = SimpleMath.multiply(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of divide method, of class SimpleMath.
+     */
+    @org.junit.Test
+    public void testDivide() {
+        System.out.println("divide");
+        int a = 4;
+        int b = 2;
+        int expResult = 2;
+        int result = SimpleMath.divide(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+    
+}
